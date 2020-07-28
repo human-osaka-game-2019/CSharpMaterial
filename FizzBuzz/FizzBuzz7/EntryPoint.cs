a5f95beba22775c5b303bda0750a912f7d8d1792
Masami Sugao <glynavenue@gmail.com>
2020-07-28 20:56:43 +0900

    :sparkles: FizzBuzz6～8追加

---
+++ FizzBuzz/FizzBuzz7/EntryPoint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace FizzBuzz7 {
+	class EntryPoint {
+
+		private const int OutputCount = 100;
+		static void Main(string[] args) {
+			// 型推論
+			var fizzListSource = CreateSourceList(3, "Fizz");
+			var buzzListSource = CreateSourceList(5, "Buzz");
+
+			// LINQ
+			// ラムダ式
+			var fizzBuzzList = fizzListSource.Join(
+									buzzListSource, 
+									fizz => fizz.index, 
+									buzz => buzz.index, 
+									(fizz, buzz) => (fizz.index, outputStr: fizz.outputStr + buzz.outputStr));
+
+			var fizzList = fizzListSource.Where(x => fizzBuzzList.Count(y => x.index == y.index) == 0);
+			var buzzList = buzzListSource.Where(x => fizzBuzzList.Count(y => x.index == y.index) == 0);
+			var numberedList = Enumerable.Range(1, OutputCount)
+				.Where(x => fizzList.Count(y => x == y.index) == 0 
+					&& buzzList.Count(y => x == y.index) == 0
+					&& fizzBuzzList.Count(y => x == y.index) == 0)
+				.Select(i => (index: i, outputStr: i.ToString()));
+
+			fizzList
+				.Concat(buzzList)
+				.Concat(fizzBuzzList)
+				.Concat(numberedList)
+				.OrderBy(x => x.index)
+				.ToList()
+				.ForEach(x => Console.WriteLine(x.outputStr));
+		}
+
+		private static List<(int index, string outputStr)> CreateSourceList(int multiplier, string outputStr) {
+			// ValueTuple
+			var ret = new List<(int, string)>();
+
+			// ローカル関数
+			int multiple(int i) => i * multiplier;
+
+			for (int i = 1; multiple(i) <= OutputCount; i++) {
+				ret.Add((multiple(i), outputStr));
+			}
+
+			return ret;
+		}
+
+	}
+}
+
---
+++ FizzBuzz/FizzBuzz8/EntryPoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz8 {
+	class EntryPoint {
+
+		static void Main(string[] args) {
+			// 型推論
+			var fizzBuzzCollection= new FizzBuzzElement[100];
+			OverwriteMessages(fizzBuzzCollection, 2, "Fizz");
+			OverwriteMessages(fizzBuzzCollection, 4, "Buzz");
+
+			// LINQ
+			// ラムダ式
+			fizzBuzzCollection.Select((x, i) => {
+				// Null合体演算子
+				x.OutputMessage ??= i.ToString();
+				return x;
+			}).ToList().ForEach(x => Console.WriteLine(x));
+		}
+
+		private static void OverwriteMessages(IEnumerable<FizzBuzzElement> target, int skipCount, string message) {
+			var skipped = target.Skip(skipCount);
+			skipped.First().OutputMessage += message;
+			OverwriteMessages(skipped.Skip(1), skipCount, message);
+		}
+
+		private class FizzBuzzElement {
+			// 自動プロパティ
+			// Null許容参照型
+			public string? OutputMessage { get; set; }
+		}
+
+	}
+}
+
